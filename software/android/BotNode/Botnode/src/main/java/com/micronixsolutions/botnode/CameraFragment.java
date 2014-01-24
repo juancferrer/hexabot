9d055baf7805eeb42890db653f01f0183a6e382d
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2014-01-24 01:13:12 -0800

    Rotates the image to fit better on the screen

--- software/android/BotNode/Botnode/src/main/java/com/micronixsolutions/botnode/CameraFragment.java
+++ software/android/BotNode/Botnode/src/main/java/com/micronixsolutions/botnode/CameraFragment.java
@@ -1,21 +1,27 @@
 package com.micronixsolutions.botnode;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
-public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2{
-    JavaCameraView mCamera;
+public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener{
+    CustomCameraView mCamera;
     Detector mDetector;
-    Mat processedImg;
+    Mat mProcessedImg;
+    private Display mDisplay;
 
     public CameraFragment() {
     }
@@ -23,13 +29,16 @@ public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvC
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+        mDisplay = ((WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         View rootView = inflater.inflate(R.layout.camera_fragment, container, false);
+        mProcessedImg = new Mat();
         mDetector = new Detector(0,255,0,255,0,255,0,0);
-        mCamera = (JavaCameraView) rootView.findViewById(R.id.camera_view);
+        mCamera = (CustomCameraView) rootView.findViewById(R.id.camera_view);
         mCamera.setVisibility(SurfaceView.VISIBLE);
         mCamera.setCvCameraViewListener(this);
         mCamera.enableView();
         mCamera.enableFpsMeter();
+        mCamera.setOnTouchListener(this);
         return rootView;
     }
     @Override
@@ -45,16 +54,16 @@ public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvC
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         //Call a native method to process image, and display that.
-        if(processedImg==null){
-            processedImg = new Mat(inputFrame.rgba().size(), inputFrame.rgba().type());
+        mDetector.detect(inputFrame.rgba(), mProcessedImg);
+        if(mDisplay.getRotation() == Surface.ROTATION_270){
+            Core.flip(mProcessedImg, mProcessedImg, -1);
         }
-        mDetector.detect(inputFrame.rgba(), processedImg);
-        return processedImg;
-        //return inputFrame.rgba();
-        /*
-        Mat hsv = new Mat();
-        Imgproc.cvtColor(inputFrame.rgba(), hsv, Imgproc.COLOR_RGB2HSV);
-        return hsv;
-        */
+        return mProcessedImg;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        Log.d("TOUCHED", "X:" + motionEvent.getX() + " Y:" + motionEvent.getY());
+        return true;
     }
 }
